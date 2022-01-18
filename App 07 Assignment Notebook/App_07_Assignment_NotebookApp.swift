5b2c1f87db1748b6bf77fabe9197059a2ddd69dc
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-01-18 10:25:34 -0600

    Initial Commit

---
+++ App 07 Assignment Notebook/App_07_Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_07_Assignment_NotebookApp.swift
+//  App 07 Assignment Notebook
+//
+//  Created by Student on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct App_07_Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
